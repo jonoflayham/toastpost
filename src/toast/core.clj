9c3de2ed0484dde8b3ac2ee2c86961fb38979956
Jon Woods <jonoflayham@gmail.com>
2017-08-25 20:51:17 +0100

    Add dummy implementation

--- src/toast/core.clj
+++ src/toast/core.clj
@@ -1,7 +1,16 @@
-(ns toast.core
-  (:gen-class))
+(ns toast.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn pretend-send-txt [number message]
+  (println "I would send the following message to" number ":")
+  (println message))
+
+(defn send-text [number message]
+  (publish :topic-arn "arn:aws:sns:us-east-1:676820690883:my-topic"
+           :subject "Toast PST"
+           :message message
+           :message-attributes {"number" number}))
+
+(defn send-toast-txt [number name place]
+  (send-txt number
+            (str "Hello " name "!  Your toast is ready for you in the " place "...")))
